71915e7aecd3c3880f761a5760c89d8f75cd5346
hyuckkim <monkeyhyuck@naver.com>
2021-12-28 10:34:39 +0900

    explanatory notes in header update.

--- anyoung/annyCore.h
+++ anyoung/annyCore.h
@@ -1,6 +1,13 @@
 #pragma once
 #include "types.h"
 
+/// <summary>
+/// Make word stack in Line input
+/// </summary>
+/// <param name="startP">pointer of line's start position</param>
+/// <param name="endP">pointer of line's end position</param>
+/// <param name="index">return only one int, return stack's length</param>
+/// <returns>sliced pointer</returns>
 stack* InitFactorSliceData(const char* startF, const char* endF, int* index);
 
 //(시작지점 startF와 종료지점 endF로 표현되는) 라인 문자열의 일부를 계산해 나타낸다.
