0aa4594cb55a14a3bfee994993d51ef359427849
hyuckkim <monkeyhyuck@naver.com>
2022-02-03 17:49:29 +0900

    Cut main function

--- anyoung/anyoung.c
+++ anyoung/anyoung.c
@@ -15,45 +15,61 @@ extern int Anyoung_Init();
 
 extern int ind;
 
+FILE* openSourceFile(int argc, char** argv) {
+	FILE* stream = NULL;
+	char argu[20] = "";
+	if (argu[0] != '\0') {
+		if (fopen_s(&stream, argu, "r") != 0 || stream == NULL) {
+			printf("파일을 여는 데 실패하였습니다.");
+			return NULL;
+		}
+		return stream;
+	}
+	else if (argc != 1) {
+		if (fopen_s(&stream, argv[1], "r") != 0 || stream == NULL) {
+			printf("파일을 여는 데 실패하였습니다.");
+			return NULL;
+		}
+		else {
+			return stream;
+		}
+	}
+	else {
+		return NULL;
+	}
+}
+char* getLine(FILE* stream, char* buffer)
+{
+	if (stream == NULL)
+	{
+		if (ind == 0) return getSO(buffer, ">>> ");
+		else return getSO(buffer, "... ");
+	}
+	else
+	{
+		return fgets(buffer, lineLength, stream);
+	}
+}
 int main(int argc, char** argv)
 {
 	function* LastF = NULL;
 	Anyoung_Init();
 	char chars[lineLength] = "default";
-	FILE* stream = NULL;
+	FILE* stream = openSourceFile(argc, argv);
 
-	int getBy;
 	char* getIn;
-	if (argc == 1)
+	if (stream == NULL)
 	{
 		anyFunction("말하기 \"어순에 상관없는 한글 스크립트 언어 안녕\"을");
 		anyFunction("말하기 \"v0.3 실행중\"을");
 		anyFunction("말하기 \"'도움'으로 명령어 확인\"을");
-		getBy = 0;
-	}
-	else
-	{
-		if (fopen_s(&stream, argv[1], "r") != 0 || stream == NULL) 
-		{
-			printf("파일을 여는 데 실패하였습니다.");
-			return 0;
-		}
-		getBy = 1;
 	}
 
 	while (1)
 	{
-		if (getBy == 0) 
-		{
-			if (ind == 0) getIn = getSO(chars, ">>> ");
-			else getIn = getSO(chars, "... ");
-			if (getIn == NULL) break;
-		}
-		else 
-		{
-			getIn = fgets(chars, lineLength, stream);
-			if (getIn == NULL) break;
-		}
+		getIn = getLine(stream, chars);
+		if (getIn == NULL) 
+			break;
 		ChangeLFtoNULL(chars);
 
 		anyFunction(chars);
